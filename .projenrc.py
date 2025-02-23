ed8d3e7ad0fd1d59604ef04b41ace293eec953be
corymhall <43035978+corymhall@users.noreply.github.com>
2025-02-23 07:57:38 -0500

    feat!: initial release

--- .projenrc.py
+++ .projenrc.py
@@ -1,14 +1,24 @@
-from projen.python import PythonProject, VenvOptions
+from projen.python import ProjenrcOptions
+from hallcor.pulumi_projen_project_types import PythonComponent
 
-project = PythonProject(
+project = PythonComponent(
     author_email="43035978+corymhall@users.noreply.github.com",
     author_name="corymhall",
     module_name="pulumi_lambda_builders",
+    component_name="lambda-builders",
     name="pulumi-lambda-builders",
     version="0.1.0",
-    deps=["pulumi>=3.149,<4.0", "aws_lambda_builders"],
-    dev_deps=["pyfakefs", "numpy"],
-    venv_options=VenvOptions(envdir="venv"),
+    deps=[
+        "pulumi>=3.149,<4.0",
+        "aws_lambda_builders",
+        "toml",
+    ],
+    projenrc_python_options=ProjenrcOptions(projen_version=">=0.91"),
+    dev_deps=[
+        "pyfakefs",
+        "numpy",
+        "hallcor.pulumi-projen-project-types",
+    ],
 )
 
 project.add_git_ignore("node_modules")
