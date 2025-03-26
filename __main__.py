b98b9f3daff2d895ec37773850a57da7ab992cb2
corymhall <43035978+corymhall@users.noreply.github.com>
2025-03-26 05:13:10 -0400

    chore: update workflows

--- __main__.py
+++ __main__.py
@@ -1,15 +1,7 @@
 from pulumi.provider.experimental import component_provider_host, Metadata
-import os
-import json
-
-dir = os.path.dirname(os.path.abspath(__file__))
-version_file_path = os.path.join(dir, "version.json")
-with open(version_file_path, "r") as f:
-    pyproject = json.load(f)
-version = pyproject["version"]
 
 if __name__ == "__main__":
     # Call the component provider host. This will discover any ComponentResource
     # subclasses in this package, infer their schema and host a provider that
     # allows constructing these components from a Pulumi program.
-    component_provider_host(Metadata("lambda-builders", version))
+    component_provider_host(Metadata("lambda-builders"))
