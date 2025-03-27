9392d404c2f867ebec686ffbec08d523604f7ca3
Cory Hall <43035978+corymhall@users.noreply.github.com>
2025-03-27 12:59:46 -0400

    chore: upgrade pulumi to 3.159.0 (#13)

--- __main__.py
+++ __main__.py
@@ -1,7 +1,28 @@
-from pulumi.provider.experimental import component_provider_host, Metadata
+from pulumi.provider.experimental import component_provider_host
+from pulumi_lambda_builders.build_custom import BuildCustomMake
+from pulumi_lambda_builders.build_dotnet import BuildDotnet
+from pulumi_lambda_builders.build_go import BuildGo
+from pulumi_lambda_builders.build_java import BuildJava
+from pulumi_lambda_builders.build_nodejs import BuildNodejs
+from pulumi_lambda_builders.build_python import BuildPython
+from pulumi_lambda_builders.build_rust import BuildRust
+from pulumi_lambda_builders.build_ruby import BuildRuby
+
 
 if __name__ == "__main__":
     # Call the component provider host. This will discover any ComponentResource
     # subclasses in this package, infer their schema and host a provider that
     # allows constructing these components from a Pulumi program.
-    component_provider_host(Metadata("lambda-builders"))
+    component_provider_host(
+        name="lambda-builders",
+        components=[
+            BuildCustomMake,
+            BuildDotnet,
+            BuildGo,
+            BuildJava,
+            BuildNodejs,
+            BuildPython,
+            BuildRust,
+            BuildRuby,
+        ],
+    )
---
+++ examples/ts-lambda/README.md
@@ -0,0 +1,15 @@
+# AWS TypeScript Lambda
+
+## Getting Started
+
+Install dependencies
+
+```console
+$ npm install
+```
+
+Deploy example
+
+```console
+$ pulumi up
+```
